c5117203672a5b42a99938cf1afa391dbb786233
axelwalter <axel.walter@uni-tuebingen.de>
2022-12-12 22:07:37 +0100

    renaming files

--- Statistics_for_Metabolomics.py
+++ Statistics_for_Metabolomics.py
@@ -4,8 +4,8 @@ from src.utils import *
 from src.stats import *
 from src.visualizations import *
 
-st.set_page_config(layout="wide")
 st.session_state.use_container_width = True
+st.set_page_config(layout="wide")
 
 st.session_state.md = pd.DataFrame()
 st.session_state.ft = pd.DataFrame()
