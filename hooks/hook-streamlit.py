b941b2158901c721e00acf4347bd3d7076170de9
axelwalter <axel.walter@uni-tuebingen.de>
2024-02-28 14:31:09 +0100

    add files for windows executable

---
+++ hooks/hook-streamlit.py
@@ -0,0 +1,11 @@
+from PyInstaller.utils.hooks import copy_metadata
+
+datas = []
+datas += copy_metadata("streamlit")
+datas += copy_metadata("plotly")
+datas += copy_metadata("pingouin")
+datas += copy_metadata("openpyxl")
+datas += copy_metadata("kaleido")
+datas += copy_metadata("scikit-posthocs")
+datas += copy_metadata("scikit-bio")
+datas += copy_metadata("gnpsdata")
