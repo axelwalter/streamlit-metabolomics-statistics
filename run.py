a6d5a77aa74b2dde512a3afb656e5f2aeeceb29b
axelwalter <axel.walter@uni-tuebingen.de>
2024-06-13 09:06:54 +0200

    windows executable embedded Python

--- run.py
+++
@@ -1,7 +0,0 @@
-from streamlit.web import cli
-
-if __name__ == "__main__":
-    cli._main_run_clExplicit(
-        file="Statistics_for_Metabolomics.py", command_line="streamlit run", args=[]
-    )
-    # we will create this function inside our streamlit framework
